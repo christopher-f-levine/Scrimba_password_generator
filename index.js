c151c56f8ba8a3f93b04617af683cbfdfaba03e2
CHRIS_LEVINE <christopher.f.levine@gmail.com>
2025-12-16 12:43:29 -0800

    Implement stretch goals

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 const specialCharacters = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+let usableCharacters = []
 
 let password1El = document.getElementById('generated-password-1')
 let password2El = document.getElementById('generated-password-2')
 
+let includeNumbersEl = document.getElementById("include-numbers-el")
+let includeSpecialCharactersEl = document.getElementById("include-characters-el")
+
+const slider = document.getElementById("length")
+const output = document.getElementById("lengthValue-el")
+
+slider.addEventListener("input", () => {
+    output.textContent = slider.value
+
+  })
+
+
+ output.textContent = slider.value;
+
 // edit the below code block to enable character restraints from toggle menu in UI
-const usableCharacters = []
-if (true) {
-    for (let i=0; i < letters.length; i++) {
-       usableCharacters.push(letters[i])
+
+function determineUsableCharacters() {
+    let includeNumbers = includeNumbersEl.checked
+    let includeSpecial = includeSpecialCharactersEl.checked
+    usableCharacters = letters
+    if (includeNumbers) {
+        for (let i=0; i < numbers.length; i++) {
+        usableCharacters.push(numbers[i])
+        }
     }
-    for (let i=0; i < numbers.length; i++) {
-       usableCharacters.push(numbers[i])
+    if (includeSpecial) { 
+        for (let i=0; i < specialCharacters.length; i++) {
+        usableCharacters.push(specialCharacters[i])
+        }
     }
-    for (let i=0; i < specialCharacters.length; i++) {
-       usableCharacters.push(specialCharacters[i])
+    return usableCharacters
     }
-}
+
 
 // generates a random integer between 1 and usable characters.length
 function generateRandomNumber() {
-    return Math.floor(Math.random() * usableCharacters.length+1)
+    return Math.floor(Math.random() * usableCharacters.length)
 }
 
 // uses the randomly generated integer to select from a variable list of usable characters
@@ -30,8 +51,8 @@ function renderRandomCharacterfromSet() {
 }
 
 // generates a password of variable length
-let passwordLength = 15
 function generateRandomPassword() {
+    let passwordLength = slider.value
     let password = ""
     for (let i = 0; i < passwordLength; i++) {
         password = password + renderRandomCharacterfromSet()
@@ -40,6 +61,7 @@ function generateRandomPassword() {
 }
 
 function renderPasswords() {
+    usableCharacters = determineUsableCharacters()
     password1El.textContent = generateRandomPassword()
     password2El.textContent = generateRandomPassword()
 }
